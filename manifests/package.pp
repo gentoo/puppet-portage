gentoo/puppet-portage

commit 48c1505fdef86b03161c7b1174217b08e36070bd

package: add to world file (and deselect on removal)
This is important anyway, but it's especially important with anything
involving subslot deps, otherwise Portage won't honour those dependencies.

Closes: https://github.com/gentoo/puppet-portage/issues/155
Signed-off-by: Sam James <sam@gentoo.org>

manifests/package.pp
@@ -300,6 +300,24 @@ define portage::package (
                     '/usr/sbin','/usr/bin','/sbin','/bin'],
   }
 
+  if($removing) {
+    exec { "emerge deselect ${atom}":
+      command => "${_emerge_command} --deselect ${atom}",
+      timeout => 43200,
+      # Emerge inherits the path, so it must be valid.
+      path    => ['/usr/local/sbin','/usr/local/bin',
+                  '/usr/sbin','/usr/bin','/sbin','/bin'],
+    }
+  else {
+    exec { "emerge noreplace ${atom}":
+      command => "${_emerge_command} --noreplace ${atom}",
+      timeout => 43200,
+      # Emerge inherits the path, so it must be valid.
+      path    => ['/usr/local/sbin','/usr/local/bin',
+                  '/usr/sbin','/usr/bin','/sbin','/bin'],
+    }
+  }
+
   package { $name:
     ensure => $ensure,
   }

commit 0db08341850e73ae20027ac6908a2fbd6818190b

package: add missing brace
Fixes: 48c1505fdef86b03161c7b1174217b08e36070bd
Signed-off-by: Sam James <sam@gentoo.org>

manifests/package.pp
@@ -308,7 +308,7 @@ define portage::package (
       path    => ['/usr/local/sbin','/usr/local/bin',
                   '/usr/sbin','/usr/bin','/sbin','/bin'],
     }
-  else {
+  } else {
     exec { "emerge noreplace ${atom}":
       command => "${_emerge_command} --noreplace ${atom}",
       timeout => 43200,